9b86c9efcc073c573c5ea4bd548ab059d4e08513
Kanisk Chakraborty <57884127+chakrakan@users.noreply.github.com>
2023-09-20 05:11:33 -0400

    update

--- utils/data-downloader.py
+++ utils/data-downloader.py
@@ -1,4 +1,6 @@
 import asyncio
+import csv
+from typing import Optional
 import aiohttp
 import json
 import gzip
@@ -11,84 +13,92 @@ from io import BytesIO
 S3_BUCKET_URL = "https://power-rankings-dataset-gprhack.s3.us-west-2.amazonaws.com"
 
 
-async def download_gzip_and_write_to_json(session, file_name):
+async def download_gzip_and_write_to_json(session, remote_file_name: str, local_file_name: Optional[str] = None):
     # If file already exists locally do not re-download game
-    if os.path.isfile(f"{file_name}.json"):
+    if os.path.isfile(f"{local_file_name}.json"):
         return
 
-    async with session.get(f"{S3_BUCKET_URL}/{file_name}.json.gz") as response:
+    async with session.get(f"{S3_BUCKET_URL}/{remote_file_name}.json.gz") as response:
         if response.status == 200:
             try:
                 gzip_bytes = BytesIO(await response.read())
                 with gzip.GzipFile(fileobj=gzip_bytes, mode="rb") as gzipped_file:
-                    with open(f"{file_name}.json", 'wb') as output_file:
+                    with open(f"{local_file_name}.json", 'wb') as output_file:
                         shutil.copyfileobj(gzipped_file, output_file)
-                print(f"{file_name}.json written")
+                print(f"{local_file_name}.json written")
             except Exception as e:
                 print("Error:", e)
         else:
-            print(f"Failed to download {file_name}")
+            print(f"Failed to download {remote_file_name}")
 
 
 async def download_esports_files(session):
-    directory = "lol-esports-data"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    local_directory = "lol-esports-data"
+    remote_directory = "esports-data"
+    if not os.path.exists(local_directory):
+        os.makedirs(local_directory)
 
     esports_data_files = ["leagues", "tournaments", "players", "teams", "mapping_data"]
-    tasks = [download_gzip_and_write_to_json(session, f"{directory}/{file_name}") for file_name in esports_data_files]
+    tasks = [download_gzip_and_write_to_json(session, f"{local_directory}/{file_name}", f"{remote_directory}/{file_name}") for file_name in esports_data_files]
     await asyncio.gather(*tasks)
 
 
-async def download_games(session, year):
+async def download_games(session, year: int, num_games: Optional[int] = None):
     start_time = time.time()
-    with open("esports-data/tournaments.json", "r") as json_file:
-        tournaments_data = json.load(json_file)
-    with open("esports-data/mapping_data.json", "r") as json_file:
-        mappings_data = json.load(json_file)
 
     directory = "games"
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    mappings = {
-        esports_game["esportsGameId"]: esports_game for esports_game in mappings_data
-    }
+    # use games that have completed and have an actual winner
+    # filter by year, for 2023, len = 7855
+    with open("esports-data/tournament-game-data.csv", "r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        completed_game_ids = [row['game_id'] for row in reader if row.get("startDate", "").startswith(str(year))]
 
+    print(len(completed_game_ids))
+    mappings = {}
+
+    # use mapping-data with cleaned up rows where we don't have info
+    # for 2023, len = 503
+    with open("esports-data/filtered-mapping-data.csv", "r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            if row['esportsGameId'] in completed_game_ids:
+                mappings[row['esportsGameId']] = row['platformGameId']
+    
     game_counter = 0
+    total_games = len(mappings)
     tasks = []
+    
+    for platform_id in mappings.values():
+        tasks.append(download_gzip_and_write_to_json(session, f"{directory}/{platform_id}", f"{directory}/{platform_id}"))
+        game_counter += 1
 
-    for tournament in tournaments_data:
-        start_date = tournament.get("startDate", "")
-        if start_date.startswith(str(year)):
-            print(f"Processing {tournament['slug']}")
-            for stage in tournament["stages"]:
-                for section in stage["sections"]:
-                    for match in section["matches"]:
-                        for game in match["games"]:
-                            if game["state"] == "completed":
-                                try:
-                                    platform_game_id = mappings[game["id"]]["platformGameId"]
-                                except KeyError:
-                                    print(f"{platform_game_id} {game['id']} not found in the mapping table")
-                                    continue
+        if game_counter % 10 == 0:
+            await asyncio.gather(*tasks)
+            tasks = []  # Resetting tasks list after awaiting
+            print(
+                f"----- Processed {game_counter} games/{total_games}, current run time: \
+                {round((time.time() - start_time)/60, 2)} minutes"
+            )
+        if game_counter == num_games:
+            print(f"----- Processed {game_counter} games, total run time: \
+            {round((time.time() - start_time)/60, 2)} minutes")
+            print("----- Downloading completed")
+            print("----- Exiting...")
+            return
 
-                                tasks.append(download_gzip_and_write_to_json(session, f"{directory}/{platform_game_id}"))
-                                game_counter += 1
-
-                            if game_counter % 10 == 0:
-                                print(
-                                    f"----- Processed {game_counter} games, current run time: \
-                                    {round((time.time() - start_time)/60, 2)} minutes"
-                                )
-    await asyncio.gather(*tasks)
+    if tasks:
+        await asyncio.gather(*tasks)
 
 
 async def main():
-    connector = aiohttp.TCPConnector(limit=10)
+    connector = aiohttp.TCPConnector(limit=5)
     async with aiohttp.ClientSession(connector=connector) as session:
-        await download_esports_files(session)
+        # await download_esports_files(session)
         await download_games(session, 2023)
+    
 
 
 if __name__ == "__main__":
--- utils/data-manipulator.py
+++ utils/data-manipulator.py
@@ -4,20 +4,25 @@ import json
 import asyncio
 import aiofiles
 import pandas as pd
+import os
+import concurrent.futures
+
 
 ESPORTS_DATA_DIR = "esports-data"
 
-async def convert_csv_to_parquet(csv_file, parquet_file):
-    async with aiofiles.open(csv_file, mode='r') as f:
-        content = await f.read()
-        df = pd.read_csv(StringIO(content))
-        df.to_parquet(parquet_file)
+# Games manipulator
+GAMES_DIR = "games"
+TARGET_DIR = "games-parquet"
 
-async def convert_json_to_parquet(json_file, parquet_file):
-    async with aiofiles.open(json_file, mode='r') as f:
-        content = await f.read()
-        df = pd.read_json(StringIO(content))
-        df.to_parquet(parquet_file)
+def convert_to_parquet(json_file):
+    source_file = os.path.join(GAMES_DIR, json_file)
+    target_file = os.path.join(TARGET_DIR, os.path.splitext(json_file)[0] + '.parquet')
+
+    df = pd.read_json(source_file)
+    df.to_parquet(target_file, engine='pyarrow')
+
+    print(f"Converted {json_file} to Parquet.")
+
 
 def read_gzipped_json(file_path):
     with gzip.open(file_path, 'rt', encoding='utf-8') as file:
@@ -32,14 +37,24 @@ def convert_parquet_to_csv(parquet_file_path, csv_file_path):
     df = pd.read_parquet(parquet_file_path)
     df.to_csv(csv_file_path, index=False)
 
-async def main():
-    convert_parquet_to_csv(f'{ESPORTS_DATA_DIR}/tournaments.parquet', f'{ESPORTS_DATA_DIR}/tournaments.csv')
-    # convert_parquet_to_csv(f'{ESPORTS_DATA_DIR}/distinct_team_player_mapping.parquet', f'{ESPORTS_DATA_DIR}/distinct_team_player_mapping.csv')
-    # data = read_gzipped_json(f'{ESPORTS_DATA_DIR}/mapping_data.json.gz')
-    # save_as_json(data, f'{ESPORTS_DATA_DIR}/mapping_data.json')
+async def convert_csv_to_parquet(csv_file, parquet_file):
+    async with aiofiles.open(csv_file, mode='r') as f:
+        content = await f.read()
+        df = pd.read_csv(StringIO(content))
+        df.to_parquet(parquet_file)
+
+def main():
+    convert_parquet_to_csv(f"{ESPORTS_DATA_DIR}/cleaned_distinct_team_player_mapping.parquet", f"{ESPORTS_DATA_DIR}/cleaned_distinct_team_player_mapping.csv")
+    # Get list of JSON files
+    # json_files = [f for f in os.listdir(GAMES_DIR) if f.endswith('.json')]
+
+    # # Convert files concurrently
+    # with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
+    #     executor.map(convert_to_parquet, json_files)
+    
+# async def main():
     # files_csv = [
-    #     (f'{ESPORTS_DATA_DIR}/mapping_data.csv', f'{ESPORTS_DATA_DIR}/mapping_data.parquet'),
-    #     (f'{ESPORTS_DATA_DIR}/distinct_team_player_mapping.csv', f'{ESPORTS_DATA_DIR}/distinct_team_player_mapping.parquet'),
+    #     (f'{ESPORTS_DATA_DIR}/tournament-game-data.csv', f'{ESPORTS_DATA_DIR}/tournament-game-data.parquet'),
     # ]
 
     # files_json = [
@@ -52,4 +67,5 @@ async def main():
     # await asyncio.gather(*tasks_json)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    # asyncio.run(main())
+    main()
